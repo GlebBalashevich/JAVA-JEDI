35ea28dbc828aebb4b4fbdfcfedfb2edd08cdcde
Gleb_Balashevich <gleb_balashevich@epam.com>
2021-04-06 09:59:50 +0300

    Initial commit

---
+++ src/com/ekids/javajedi/homework1/TaskHome1.java
@@ -0,0 +1,22 @@
+package com.ekids.javajedi.homework1;
+
+/**
+ * Создать 5 ситхов, 3 джедая, 3 падавана.
+ * Дать им всем уникальные имена
+ */
+
+public class TaskHome1 {
+
+
+    public static class Sith {
+        public String name;
+    }
+
+    public static class Jedi {
+        public String name;
+    }
+
+    public static class Padawan {
+        public String name;
+    }
+}
---
+++ src/com/ekids/javajedi/lesson1/Task2.java
@@ -0,0 +1,10 @@
+package com.ekids.javajedi.lesson1;
+
+/**
+ * Добавь метод public static void printString(String s), в нем напиши код,
+ * который будет выводить переданную строку на экран.
+ */
+
+public class Task2 {
+
+}
